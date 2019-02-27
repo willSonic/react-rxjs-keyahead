e2dad1d2e33a7ffdf9e5641022a24e39b6991d99
Will Streter <will@obo.pm>
2019-02-26 17:27:37 -0800

    Initial commit  files

--- src/view-layer/_root/App.jsx
+++ src/view-layer/_root/App.jsx
@@ -10,29 +10,28 @@ import Theme from '../mui-modules/muiTheme';
 
 const THEME = createMuiTheme(Theme);
 class App extends Component {
-
   render() {
     return (
-    <MuiThemeProvider theme={THEME}>
-      <Provider store={ store }>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.jsx</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-      </Provider>
-    </MuiThemeProvider>
+      <MuiThemeProvider theme={THEME}>
+        <Provider store={store}>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.jsx</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          </div>
+        </Provider>
+      </MuiThemeProvider>
     );
   }
 }
